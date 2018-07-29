fd91f4ba0efa9f220012d073ea45633e462a1300
TKNGUE <takeno@jnlp.org>
2018-07-29 21:03:36 +0900

    [ADD] Rust: 3.2. Dining Philosophers

---
+++ codes/rust/dining_philosopheres/src/main.rs
@@ -0,0 +1,65 @@
+
+use std::thread;
+use std::time::Duration;
+use std::sync::{Mutex, Arc};
+
+struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize,
+}
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+impl Philosopher {
+    pub fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right,
+        }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} is eating.", self.name);
+        thread::sleep(Duration::from_millis(1000));
+        println!("{} is done eating.", self.name);
+    }
+}
+
+fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
+    let philosopheres = vec![
+        Philosopher::new("Jutith Butler", 0, 1),
+        Philosopher::new("Gilles DEleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Emma Goldman", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4),
+    ];
+
+    let handles: Vec<_> = philosopheres.into_iter().map(|p| {
+        let table = table.clone();
+
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+
+}
