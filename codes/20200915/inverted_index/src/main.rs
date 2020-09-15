caf85a58f166daa8f980934109cf0ca3c42a2c6e
TKNGUE <takegue@gmail.com>
2020-09-15 20:04:01 +0900

    Impl. file reader

--- codes/20200915/inverted_index/src/main.rs
+++ codes/20200915/inverted_index/src/main.rs
@@ -1,6 +1,67 @@
 extern crate argparse;
 
 use argparse::{ArgumentParser, Collect, StoreTrue};
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::PathBuf;
+use std::sync::mpsc::{channel, Receiver};
+use std::thread::{spawn, JoinHandle};
+
+fn start_file_reader_thread(
+    documents: Vec<PathBuf>,
+) -> (Receiver<String>, JoinHandle<io::Result<()>>) {
+    let (sender, reciver) = channel();
+    let handle = spawn(move || {
+        for filename in documents {
+            let mut f = File::open(filename)?;
+            let mut text = String::new();
+            f.read_to_string(&mut text)?;
+            if sender.send(text).is_err() {
+                break;
+            }
+        }
+        Ok(())
+    });
+
+    (reciver, handle)
+}
+
+fn run_in_parrallel(filenames: Vec<String>) -> io::Result<()> {
+    // let output_dir = PathBuf::from(".");
+    let documents = expand_filename_argumentrs(filenames)?;
+    let _ = start_file_reader_thread(documents);
+
+    Ok(())
+}
+
+fn expand_filename_argumentrs(args: Vec<String>) -> io::Result<Vec<PathBuf>> {
+    let mut filenames = vec![];
+    for arg in args {
+        let path = PathBuf::from(arg);
+        if path.metadata()?.is_dir() {
+            for entry in path.read_dir()? {
+                let entry = entry?;
+                if entry.file_type()?.is_file() {
+                    filenames.push(entry.path());
+                } else {
+                    unimplemented!();
+                }
+            }
+        } else {
+            filenames.push(path);
+        }
+    }
+    Ok(filenames)
+}
+
+fn run(filenames: Vec<String>, single_threaded: bool) -> io::Result<()> {
+    if single_threaded {
+        unimplemented!();
+    } else {
+        run_in_parrallel(filenames)
+    }
+}
 
 fn main() {
     let mut single_threaded = false;
@@ -24,5 +85,8 @@ fn main() {
         ap.parse_args_or_exit();
     }
 
-    println!("{:?} {:?}", single_threaded, filenames);
+    match run(filenames, single_threaded) {
+        Ok(()) => {}
+        Err(err) => eprintln!("{}", err),
+    }
 }
