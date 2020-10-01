6d66b4fd1aa213300d11a5efff776fc0223979d3
TKNGUE <takegue@gmail.com>
2020-10-02 03:07:38 +0900

    Save point & Missing project files

---
+++ codes/20201002/gapbuffer/src/main.rs
@@ -0,0 +1,11 @@
+mod gap;
+
+use crate::gap::GapBuffer;
+
+fn main() {
+    let mut buf = GapBuffer::new();
+    buf.insert_iter("Lord of thr Rings".chars());
+    buf.set_position(12);
+
+    buf.insert_iter("Onion".chars());
+}
