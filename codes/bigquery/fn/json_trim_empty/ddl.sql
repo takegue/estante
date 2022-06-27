a9fb4c7661620bb9a1cdf53fb4593a7c8b573c88
takegue <takegue@gmail.com>
2022-06-28 07:09:25 +0900

    Add bigquery util: json_trim_empty

---
+++ codes/bigquery/fn/json_trim_empty/ddl.sql
@@ -0,0 +1,42 @@
+create or replace function `fn.json_trim_empty`(json string)
+returns string
+language js
+as """
+  const obj = JSON.parse(json);
+  function trimNullElements(obj) {
+      if (obj === null) {
+          return null;
+      }
+      if (Array.isArray(obj)) {
+          return obj.map(trimNullElements).filter(v => v);
+      }
+      if (typeof obj === 'object') {
+          const newObj = {};
+          for (const key in obj) {
+              if (obj.hasOwnProperty(key)) {
+                  const value = obj[key];
+                  if (value !== null) {
+                      newValue = trimNullElements(value);
+                      if(newValue !== null) {
+                          newObj[key] = newValue;
+                      }
+                  }
+              }
+          }
+          if(Object.keys(newObj).length === 0) {
+              return null;
+          }
+          return newObj;
+      }
+      return obj;
+  }
+
+  const ret = trimNullElements(obj);
+  return ret ? JSON.stringify(ret) : null;
+"""
+;
+
+assert `fn.json_trim_empty`('{"key": null}') is null;
+assert '{"key2":2}' = `fn.json_trim_empty`('{"key": null, "key2": 2}');
+assert '{"key2":2}' = `fn.json_trim_empty`('{"key": {"item": null}, "key2": 2}');
+assert '[1,2]' = `fn.json_trim_empty`('[1,null,2]');
