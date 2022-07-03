daa888560fa414bc8ca7ecbd2d7ecb1202a30217
takegue <takegue@gmail.com>
2022-07-03 12:26:07 +0900

    update description

---
+++ codes/bigquery/fn/get_query_referenced_tables/ddl.sql
@@ -0,0 +1,33 @@
+create or replace procedure `fn.get_view_referenced_tables`(
+  in query string
+  , out ret array<struct<project_id string, dataset_id string, table_id string>>
+)
+options(
+  description='''Get referenced tables from query
+
+Arguments
+=====
+  - query: a query to be analyzed. Recommended to use no-scan bytes query or query raising error to avoid billing.
+  - ret: return value. referenced_tables
+  '''
+)
+begin
+
+  declare ret array<struct<project_id string, dataset_id string, table_id string>>;
+  declare last_job_id string;
+
+  begin
+    execute immediate query
+  exception when error then
+  end;
+  set last_job_id = @@last_job_id;
+
+  set ret = (
+    select as value
+      referenced_tables
+    from `region-us.INFORMATION_SCHEMA.JOBS_BY_USER`
+    where job_id = last_job_id
+    and date(creation_time) = current_date()
+    order by start_time desc
+  );
+select ret;
