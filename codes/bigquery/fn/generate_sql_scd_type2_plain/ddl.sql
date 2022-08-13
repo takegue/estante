e68f2d78b0f6eee19ced581f446849bee0b8b94d
takegue <takegue@gmail.com>
2022-08-13 22:00:03 +0900

    Add SQL generator: SCD Type2

---
+++ codes/bigquery/fn/generate_sql_scd_type2_plain/ddl.sql
@@ -0,0 +1,95 @@
+create or replace procedure `fn.generate_sql_scd_type2_plain`(
+  out ret struct<
+    ddl string
+   , dml string
+  >
+  , destination struct<
+    project_id string
+    , dataset_id string
+    , table_id string
+  >
+  , source struct<
+    project_id string
+    , dataset_id string
+    , table_id string
+  >
+  , exp_unique_key string
+)
+begin
+  declare snapshot_query, destination_ref, source_ref string;
+
+  set (destination_ref, source_ref) = (
+    ifnull(
+      format(
+        '%s.%s.%s'
+        , coalesce(destination.project_id, @@project_id)
+        , destination.dataset_id
+        , destination.table_id
+      ), error(format("Invalid Destination: %t", destination))
+    )
+    , ifnull(
+      format(
+        '%s.%s.%s',
+        coalesce(source.project_id, @@project_id)
+        , source.dataset_id
+        , source.table_id
+      ), error(format("Invalid Source: %t", source)))
+  );
+
+  set snapshot_query = ifnull(
+    format("""
+      -- Snapshot Templated Query
+      select
+        %s as unique_key
+        , version_hash
+        , @timestamp as valid_from
+        , timestamp(null) as valid_to
+        , entity
+      from
+        `%s` as entity
+        , (select as value generate_uuid()) as version_hash
+      """
+      , exp_unique_key
+      , source_ref
+    )
+    , error(format("Invalid argument: %t", source))
+  );
+
+  set ret = (
+    -- DDL Query
+    format("""
+        create table if not exists `%s`
+        partition by DATE(valid_to)
+        cluster by unique_key
+        as %s
+      """
+      , destination_ref
+      , snapshot_query
+    )
+    -- DML Query
+    , format("""
+        merge `%s` T
+        using
+          (
+            %s
+          ) as S
+        on
+          T.valid_to is null
+          and S.unique_key = T.unique_key
+          and format('%%t', S.entity) = format('%%t', T.entity)
+        -- Insert new records changed
+        when not matched then
+          insert row
+        -- Deprecate old records changed
+        when
+          not matched by source
+          and T.valid_to is null then
+            update set
+              valid_to = @timestamp
+      """
+      , destination_ref
+      , snapshot_query
+    )
+  );
+
+end
